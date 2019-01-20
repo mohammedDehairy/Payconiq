6a372c21035f2b7c6ed5226cc0b11dfc86dbaf08
Mohamed Mohamed El Dehairy <m.eldehairy@gmail.com>
2019-01-20 11:19:36 +0100

    gitignore the workspace file and removed from tracking

--- ci_ui_tests.sh
+++ ci_ui_tests.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+pod repo update
+pod install
 xcodebuild -workspace Payconiq.xcworkspace -scheme PayconiqUITests -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone X,OS=12.1' test
--- ci_unit_tests.sh
+++ ci_unit_tests.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+pod repo update
+pod install
 xcodebuild -workspace Payconiq.xcworkspace -scheme Payconiq -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone X,OS=12.1' test
