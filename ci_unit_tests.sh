922e5dfa8bb04d5490f34809c0ffd4360b651b57
Mohamed Mohamed El Dehairy <m.eldehairy@gmail.com>
2019-01-20 13:42:49 +0100

    small change to the test scripts

--- ci_unit_tests.sh
+++ ci_unit_tests.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-pod repo update
-pod install
+./init_project.sh
 xcodebuild -workspace Payconiq.xcworkspace -scheme Payconiq -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone X,OS=12.1' test
