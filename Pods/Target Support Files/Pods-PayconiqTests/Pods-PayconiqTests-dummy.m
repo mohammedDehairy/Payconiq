e32997af2d96234233fceeefea4505a2b971c141
Mohamed Mohamed El Dehairy <m.eldehairy@gmail.com>
2019-01-20 11:13:03 +0100

    removed Pods directory and the podfile.lock file

--- Pods/Target Support Files/Pods-PayconiqTests/Pods-PayconiqTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PayconiqTests : NSObject
-@end
-@implementation PodsDummy_Pods_PayconiqTests
-@end
--- Pods/Target Support Files/Pods-PayconiqUITests/Pods-PayconiqUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PayconiqUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PayconiqUITests
-@end
